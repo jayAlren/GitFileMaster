bea465de2bfeda9356d7c67db6650e123e02b219
jayAlren <alrenjaygabasan@gmail.com>
2023-04-13 08:03:38 +0800

    for clarification

--- Untitled-1.js
+++ Untitled-1.js
@@ -2,6 +2,18 @@
 
 console.log('Hello World!');
 
-for (let i = 0; i < 5; i++) {
-  console.log('Hello');
+function thisFunc(x) {
+  for (let i = 1; i <= x; i++) {
+    if (i % 3 == 0 && i % 5 == 0) {
+      console.log('Bull Doge');
+    } else if (i % 5 == 0) {
+      console.log('Dog');
+    } else if (i % 3 == 0) {
+      console.log('Bull');
+    } else {
+      console.log(i);
+    }
+  }
 }
+
+thisFunc(50);
